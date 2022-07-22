fb18aca1f0d80dc7f20c7475bf56d85fca038d9d
willy <programmingwang@gmail.com>
2022-07-22 16:36:14 +0800

    feat: add Button and Card UI components

---
+++ src/components/UI/Button.js
@@ -0,0 +1,22 @@
+import classes from "./Button.module.css";
+import PropTypes from "prop-types";
+
+const Button = (props) => {
+  console.log(props);
+  return (
+    <button
+      onClick={props.onClick}
+      className={`${classes.button} + ${props.className}`}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.string,
+  onClick: PropTypes.func,
+  className: PropTypes.string,
+};
+
+export default Button;
---
+++ src/components/UI/Card.js
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+import classes from "./Card.module.css";
+
+const Card = (props) => {
+  console.log(props);
+  return (
+    <div className={`${classes.card} + ${props.className}`}>
+      {props.children}
+    </div>
+  );
+};
+
+Card.propTypes = {
+  children: PropTypes.object,
+  className: PropTypes.string,
+};
+
+export default Card;
